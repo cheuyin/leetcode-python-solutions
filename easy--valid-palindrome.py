f1da84dc511d135168dbf237d8f8e7a3bcf710e6
cheuyin <yinstanleycheung@gmail.com>
2022-10-07 20:59:00 -0700

    Add NeetCode's second solution

--- easy--valid-palindrome.py
+++ easy--valid-palindrome.py
@@ -1,17 +1,48 @@
-# NeetCode's solution: 
+# NeetCode's solution #1:
 
 # Time complexity: O(n)
 # Space complexity: O(n)
 
+# class Solution:
+#     def isPalindrome(self, s: str) -> bool:
+#         newStr = ""
+#         for char in s:
+#             if char.isalnum():
+#                 newStr += char.lower()
+
+#         return newStr == newStr[::-1]
+
+
+# test = Solution()
+# print(test.isPalindrome("A man, a plan, a canal: Panama."))
+
+
+# NeetCode's solution #2
+
 class Solution:
     def isPalindrome(self, s: str) -> bool:
-        newStr = ""
-        for char in s:
-            if char.isalnum():
-                newStr += char.lower()
+        l = 0
+        r = len(s) - 1
 
-        return newStr == newStr[::-1] 
+        while l < r:
+            while not self.isAlphanumeric(s[l]) and l < r:
+                l += 1
+            while not self.isAlphanumeric(s[r]) and r > l:
+                r -= 1
+
+            if s[l].lower() != s[r].lower():
+                return False
+            
+            l += 1
+            r -= 1
+        
+        return True
+
+    def isAlphanumeric(self, c: str) -> bool:
+        return ((ord("A") <= ord(c) <= ord("Z")) or
+                (ord("a") <= ord(c) <= ord("z")) or
+                (ord("0") <= ord(c) <= ord("9")))
 
 
 test = Solution()
-print(test.isPalindrome("A man, a plan, a canal: Panama.s"))
+print(test.isPalindrome(",,,,"))
