001026d44cbb466dc8d6e93833fb1f685af8292e
cheuyin <yinstanleycheung@gmail.com>
2022-10-26 21:48:38 -0700

    Add solution

---
+++ easy--number-of-one-bits.py
@@ -0,0 +1,12 @@
+# Time: O(1)
+# Space: O(1)
+
+class Solution:
+    def hammingWeight(self, n: int) -> int:
+        res = 0
+
+        while n: 
+            res += n % 2
+            n = n >> 1
+        
+        return res
