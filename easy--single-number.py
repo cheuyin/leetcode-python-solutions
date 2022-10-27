0a2df84c0d0f691f977f5cec489f4bdf6ddf37db
cheuyin <yinstanleycheung@gmail.com>
2022-10-26 21:19:13 -0700

    Add solution

---
+++ easy--single-number.py
@@ -0,0 +1,10 @@
+# Time: O(n)
+# Space: O(1)
+
+class Solution:
+    def singleNumber(self, nums: List[int]) -> int:
+        res = 0
+        for n in nums:
+            res = n ^ res  
+        
+        return res
